89ad38d75dae102798fd1eca4a3f189f7ab9e2c5
Jessica Breedlove Reece <36556575+jessicabreedlove@users.noreply.github.com>
2022-02-07 14:04:34 -0700

    Add files via upload
    jumper starting files

---
+++ Jumper/game/display.py
@@ -0,0 +1,33 @@
+
+#from game.terminal-service import terminal-service
+
+# Displays the current state of parachute and guessed letters
+# Kimberly
+
+#can just make it an array, then pop off 
+# a list of strings like
+
+#glider = [
+# '   ___'.
+#'  /     \',
+# '  ____   ']
+
+# class parachute:
+#   def __init__(self):
+
+        # self.terminalService = terminal-service()
+#       self.glider = 
+# [
+#                '   ___'.
+#               '  /     \',
+#                '  ____   ']
+
+#   def print_parachute(self):
+        # for i in self.glider:
+          # self.terminalService.write_text(i)
+
+#   def remove_parachute_piece(self):
+      # self.glider.pop(0)
+
+
+#another method or two, to check parachute etc.
